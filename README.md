6ddc7c3d2dc18c3a9a741ee6980796d3071d91e8
Gabe Sumner <gsumner@salesforce.com>
2022-06-23 16:36:19 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cumulus Mortgage Salesforce Demo
+
+Quick attempt to get Financial Services Cloud running with SFDX inside a Scratch Org.
+
+Currently incomplete.
---
+++ orgInit.sh
@@ -0,0 +1,30 @@
+# Create the scratch org (uncomment for local development)
+# sfdx force:org:delete -u mortgagedemo
+sfdx force:org:create -f config/project-scratch-def.json --setalias mortgagedemo --setdefaultusername
+
+# Create the scratch org (uncomment for the SFDX Deployer)
+# sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s -n
+
+# Push the metadata into the new scratch org.
+sfdx force:source:push
+
+# Grant user Financial Services Cloud licenses
+sfdx force:user:permsetlicense:assign -n "Financial Services Cloud Standard"
+sfdx force:user:permsetlicense:assign -n "Financial Services Cloud Extension"
+sfdx force:user:permsetlicense:assign -n "Mortgage"
+
+# Assign the permset to the default admin user.
+sfdx force:user:permset:assign -n cumulus
+
+# Import the data required by the demo
+# sfdx texei:data:export --objects Account,Contact,Opportunity,ResidentialLoanApplication --outputdir ./data
+sfdx texei:data:import --inputdir ./data
+
+# Activate the custom theme.
+sfdx shane:theme:activate -n CumulusMortgage
+
+# Set the default password.
+sfdx shane:user:password:set -g User -l User -p salesforce1
+
+# Open the demo org.
+sfdx force:org:open
